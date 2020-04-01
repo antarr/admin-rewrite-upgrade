9bbc4f910b5d8b23fb28009acabe2b048be912fd
wesley974 <milo974@gmail.com>
2020-04-01 09:58:30 +0400

    no need anymore greylist

--- app/helpers/greylist/greylisted_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Greylist::GreylistedHelper
-  def updated_at_column(record)
-    time_ago_in_words(record.updated_at) + ' ' + t('ago')
-  end
-end
--- app/helpers/greylist/whitelist_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Greylist::WhitelistHelper
-  def description_form_column(_record, _options)
-    text_area(:record, :description, size: '60x2', class: 'accepted_filetypes-input text-input as-form-wide')
-  end
-end
