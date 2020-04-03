3c68655abde523511556d817cad8bfafdcc630bc
wesley974 <milo974@gmail.com>
2020-04-03 10:22:40 +0400

    auto increment users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.connection.execute("ALTER TABLE users AUTO_INCREMENT = 2000;")
